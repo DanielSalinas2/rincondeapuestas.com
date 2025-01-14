aee3bd774a925dc02ce13ebf7cc3b75a5943b211
DanielSalinas2 <166958080+DanielSalinas2@users.noreply.github.com>
2025-01-15 00:09:54 +0100

    Primera version

---
+++ assets/scripts/email.php
@@ -0,0 +1,50 @@
+<?php
+$name = trim($_POST['contact-name']);
+$phone = trim($_POST['contact-phone']);
+$email = trim($_POST['contact-email']);
+$message = trim($_POST['contact-message']);
+if ($name == "") {
+    $msg['err'] = "\n Este campo no puede estar vacío";
+    $msg['field'] = "contact-name";
+    $msg['code'] = FALSE;
+} else if ($phone == "") {
+    $msg['err'] = "\n Phone number can not be empty!";
+    $msg['field'] = "contact-phone";
+    $msg['code'] = FALSE;
+} else if (!preg_match("/^[0-9 \\-\\+]{4,17}$/i", trim($phone))) {
+    $msg['err'] = "\n Please put a valid phone number!";
+    $msg['field'] = "contact-phone";
+    $msg['code'] = FALSE;
+} else if ($email == "") {
+    $msg['err'] = "\n Este campo no puede estar vacío";
+    $msg['field'] = "contact-email";
+    $msg['code'] = FALSE;
+} else if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+    $msg['err'] = "\n Please put a valid email address!";
+    $msg['field'] = "contact-email";
+    $msg['code'] = FALSE;
+} else if ($message == "") {
+    $msg['err'] = "\n Message can not be empty!";
+    $msg['field'] = "contact-message";
+    $msg['code'] = FALSE;
+} else {
+    $to = 'daniel@rincondeapuestas.com';
+    $subject = 'Nuevo Formulario de Rincondeapuestas.com';
+    $_message = '<html><head></head><body>';
+    $_message .= '<p>Name: ' . $name . '</p>';
+    $_message .= '<p>Message: ' . $phone . '</p>';
+    $_message .= '<p>Email: ' . $email . '</p>';
+    $_message .= '<p>Message: ' . $message . '</p>';
+    $_message .= '</body></html>';
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'From:  Papr <daniel@rincondeapuestas.com>' . "\r\n";
+    $headers .= 'cc: rincondelpickpyton@gmail.com' . "\r\n";
+    $headers .= 'bcc: danielsal.6801@gmail.com' . "\r\n";
+    mail($to, $subject, $_message, $headers, '-f daniel@rincondeapuestas.com');
+
+    $msg['success'] = "\n Email enviado correctamente.";
+    $msg['code'] = TRUE;
+}
+echo json_encode($msg);
